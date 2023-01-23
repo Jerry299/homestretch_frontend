caf4d1f1a24d112267b0020700cf6ce728b94d76
behnam1369 <behnam.aghaali@yahoo.com>
2023-01-23 22:09:01 +0330

    Add sign in and sign up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home/Home"
+import Home from "./pages/Home/Home";
+import Signin from "./pages/Signin/Signin";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
-	return <div>
-		<Home />
-	</div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign_up" element={<Signup />} />
+        <Route path="/sign_in" element={<Signin />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
