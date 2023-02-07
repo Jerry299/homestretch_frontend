690d92b50fe6589c767f2df534f053c89c348b3a
behnam1369 <behnam.aghaali@yahoo.com>
2023-02-07 19:23:17 +0330

    Create resource links on the menu

--- src/pages/Home/Menu.js
+++ src/pages/Home/Menu.js
@@ -1,14 +1,73 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { BiChevronDown } from "react-icons/bi";
 import style from "./Menu.module.scss";
+
 export default function Menu(props) {
   const user = useSelector((state) => state.user);
+  const [resources, setResources] = useState([]);
+  const token = localStorage.getItem("token");
+  const [showResources, setShowResources] = useState(false);
+  const ul = useRef(null);
+  const resourceList = useRef(null);
+  const [rect, setRect] = useState({ x: 0, y: 0, width: 0, height: 0 });
+  const [ulStyle, setUlStyle] = useState({
+    top: rect.y + rect.height + window.scrollY,
+    left: rect.x - props.dir === "ltr" ? 0 : 15,
+    maxHeight: props.maxHeight + "px",
+  });
+
+  useOutsideAlerter(ul, () => {
+    setShowResources(false);
+  });
+
+  useEffect(() => {
+    const getRes = async () => {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", token);
+
+      fetch(`https://homestretch-api.onrender.com/contents/resource`, {
+        headers: myHeaders,
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          setResources(
+            result.map((res) => {
+              return { id: res.id, title: res.title };
+            })
+          );
+        })
+        .catch((error) => console.log("error", error));
+    };
+    getRes();
+  }, [token]);
+
+  const handleResourceMenuClick = (e) => {
+    e.preventDefault();
+    let rect = resourceList.current.getBoundingClientRect();
+    setRect({ rect });
+    setUlStyle({
+      ...ulStyle,
+      top: rect.y + rect.height + window.scrollY,
+      left: rect.x,
+    });
+    setShowResources(true);
+  };
+
   return (
     <nav className={style.main}>
       <Link className={style.logo} to="/"></Link>
       <p className={`${style.links} ${props.alt ? style.alt : ""}`}>
         <Link to="/contents/education">Learning center</Link>
-        <Link to="/contents/education">Learning center</Link>
+        <Link
+          to=""
+          onClick={(e) => handleResourceMenuClick(e)}
+          ref={resourceList}
+        >
+          Resources
+          <BiChevronDown />
+        </Link>
         <Link to="/contents/education">Blog</Link>
         <Link to="/contents/education">About</Link>
         <Link to="/contents/education">Contact</Link>
@@ -22,6 +81,39 @@ export default function Menu(props) {
         }
         alt="avatar"
       />
+      {showResources && (
+        <ul className={style.ul} ref={ul} style={ulStyle}>
+          {resources.map((item, i) => {
+            return (
+              <li key={item.id}>
+                <Link
+                  to={`/contents/resource/${item.id}/${item.title.replace(
+                    /[^a-z0-9]/gi,
+                    "-"
+                  )}`}
+                  onClick={() => setShowResources(false)}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </nav>
   );
+}
+
+function useOutsideAlerter(ref, callback) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, callback]);
 }
