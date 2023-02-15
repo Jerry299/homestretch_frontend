7b3cef4948fda2b7e3b8153b9fe5aaed3ecf3678
behnam1369 <behnam.aghaali@yahoo.com>
2023-02-15 20:29:23 +0330

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Content from "./pages/Content/Content";
@@ -11,12 +11,15 @@ import Signin from "./pages/Signin/Signin";
 import Confirmation from "./pages/Signup/Confirmation";
 import ConfirmNeeded from "./pages/Signup/ConfirmNeeded";
 import Signup from "./pages/Signup/Signup";
+import OtherContent from "./pages/Content/OtherContent";
 import { updateUser } from "./redux/reducers/user";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
+  const userData = useSelector((state) => state.user);
   useEffect(() => {
-    const token = localStorage.getItem("token");
     if (token) {
       const fetchUser = async () => {
         var myHeaders = new Headers();
@@ -41,7 +44,7 @@ function App() {
 
       fetchUser();
     }
-  }, [dispatch]);
+  }, [dispatch, token]);
 
   return (
     <div className="app">
@@ -50,14 +53,45 @@ function App() {
         <Route path="/sign_up" element={<Signup />} />
         <Route path="/sign_in" element={<Signin />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/cms/:content_type" element={<ContentManagement />} />
+        {userData && userData.user.role === "admin" && (
+          <Route
+            path="/cms/:content_type"
+            element={
+              userData && userData.user.role === "admin" ? (
+                <ContentManagement />
+              ) : (
+                <PageNotFound />
+              )
+            }
+          />
+        )}
         <Route path="/contents/:content_type" element={<ContentsList />} />
         <Route path="/confirm_account/:token" element={<Confirmation />} />
         <Route path="/confirm_your_account" element={<ConfirmNeeded />} />
+        <Route path="/blog" element={<OtherContent content_type="blog" />} />
+        <Route path="/about" element={<OtherContent content_type="about" />} />
+        <Route
+          path="/contact"
+          element={<OtherContent content_type="contact" />}
+        />
+        <Route path="/faq" element={<OtherContent content_type="faq" />} />
+        <Route
+          path="/career"
+          element={<OtherContent content_type="career" />}
+        />
+        <Route
+          path="/terms-of-use"
+          element={<OtherContent content_type="terms-of-use" />}
+        />
+        <Route
+          path="/privacy-policy"
+          element={<OtherContent content_type="privacy-policy" />}
+        />
         <Route
           path="/contents/:content_type/:id/:title"
           element={<Content />}
         />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
