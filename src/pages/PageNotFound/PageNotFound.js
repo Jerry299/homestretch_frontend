d9712f6a8391d4880b6de72c0cbcae443d248c58
behnam1369 <behnam.aghaali@yahoo.com>
2023-02-16 18:15:20 +0330

    Create Page not found component

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import Footer from "../Home/Footer";
+import Menu from "../Home/Menu";
+import style from "./PageNotFound.module.scss";
+
 export default function PageNotFound() {
   return (
     <div>
-      <h1>Error 404 - Page not found</h1>
+      <Menu alt="true" />
+      <div className={style.main}>
+        <img src="page_not_found.svg" alt="404" sty />
+        <h2>Oops Page Not Found!!!</h2>
+        <p>
+          Sorry, the page you’re looking for doesn’t exist or has been moved but
+          have no fear, we’re working on it.
+        </p>
+        <Link to="/" className={style.button}>
+          Go Back to Homepage
+        </Link>
+      </div>
+      <Footer />
     </div>
   );
 }
